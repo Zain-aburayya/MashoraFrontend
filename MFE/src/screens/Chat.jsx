b9feb5b09a0a67e453b51dff7781ad8ed893fe51
Zain aburayya <zaaburayya20@cit.just.edu.jo>
2024-05-25 01:58:23 +0300

    edit Gifted chat style

--- MFE/src/screens/Chat.jsx
+++ MFE/src/screens/Chat.jsx
@@ -1,17 +1,40 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React, {useCallback, useEffect, useState} from 'react';
-import {GiftedChat} from 'react-native-gifted-chat';
+import {GiftedChat, Bubble} from 'react-native-gifted-chat';
 import {useRoute} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import firestore from '@react-native-firebase/firestore';
 import uuid from 'react-native-uuid';
 
-const Chat=()=>{
-  const[messageList,setMessageList]=useState([]);
-  const route=useRoute();
-  console.log("Data.UserId: " , route.params.data.userId);
-  useEffect(()=>{
-      const subscriber = firestore()
+const Chat = () => {
+  const [messageList, setMessageList] = useState([]);
+  const route = useRoute();
+  console.log('Data.UserId: ', route.params.data.userId);
+
+  const renderBubble = props => (
+    <Bubble
+      {...props}
+      textStyle={{
+        right: {
+          color: 'white',
+        },
+        left: {
+          color: 'black',
+        },
+      }}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#8A6F42',
+        },
+        left: {
+          backgroundColor: '#CEC3B0',
+        },
+      }}
+    />
+  );
+
+  useEffect(() => {
+    const subscriber = firestore()
       .collection('chats')
       .doc(route.params.id + route.params.data.userId)
       .collection('messages')
@@ -22,40 +45,77 @@ const Chat=()=>{
       });
       setMessageList(allmessages);
     });
-  },[]);
+  }, []);
 
-  const onSend=useCallback(async (messages=[])=>{
-      const msg=messages[0];
-      const myMsg={
-        ...msg,
-        sendBy: route.params.id,
-        sendTo: route.params.data.userId,
-        createdAt: Date.parse(msg.createdAt),
-      };
-      setMessageList(previousMessages =>
-          GiftedChat.append(previousMessages,messages),
-      );
-      firestore()
-        .collection('chats')
-        .doc(''+ route.params.id + route.params.data.userId)
-        .collection('messages')
-        .add(myMsg);
-      firestore()
-        .collection('chats')
-        .doc(''+ route.params.data.userId +route.params.id)
-        .collection('messages')
-        .add(myMsg);
-  },[]);
-  return(
+  const onSend = useCallback(async (messages = []) => {
+    const msg = messages[0];
+    const myMsg = {
+      ...msg,
+      sendBy: route.params.id,
+      sendTo: route.params.data.userId,
+      createdAt: Date.parse(msg.createdAt),
+    };
+    setMessageList(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    );
+    firestore()
+      .collection('chats')
+      .doc('' + route.params.id + route.params.data.userId)
+      .collection('messages')
+      .add(myMsg);
+    firestore()
+      .collection('chats')
+      .doc('' + route.params.data.userId + route.params.id)
+      .collection('messages')
+      .add(myMsg);
+  }, []);
+  return (
     <View style={{flex: 1}}>
+      <View style={styles.topTab}>
+        <Text style={{fontSize: 22, fontWeight: 'bold', alignItems: 'center'}}>
+          {route.params.data.username}
+        </Text>
+      </View>
       <GiftedChat
         messages={messageList}
-        onSend={messages=>onSend(messages)}
+        onSend={messages => onSend(messages)}
         user={{
-            _id: route.params.id,
+          _id: route.params.id,
         }}
+        renderBubble={renderBubble}
       />
     </View>
   );
-}   
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  bottomTab: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: 75,
+    backgroundColor: '#F2F2F2',
+    borderColor: 'rgba(110, 101, 85, 0.3)',
+    borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  topTab: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: 60,
+    backgroundColor: '#DAD2C5',
+    borderColor: 'rgba(110, 101, 85, 0.3)',
+    borderWidth: 1,
+    flexDirection: 'row-reverse',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default Chat;
